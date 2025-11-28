ee699df4fd60390bfb3c2f72878ca9a58b747c19
Poyuan <poyuan510214@gmail.com>
2025-11-28 17:20:17 +0800

    feat: add Project page

---
+++ app/project/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React from 'react'
+
+import { FeaturedProjects } from '@/components/Home/FeaturedProjects';
+import { CustomCursor } from '@/components/Home/CustomCursor';
+
+
+export default function project() {
+  return (
+    <div className="relative min-h-screen text-custom-black font-sans overflow-hidden selection:bg-custom-orange selection:text-white">
+      {/* Global Grain Texture */}
+      <div className="noise-overlay" />
+
+      {/* Ambient Background Light (Fixed behind content) */}
+      <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden">
+        <div className="absolute top-[-20%] left-[-10%] w-[60vw] h-[60vw] bg-custom-blue/15 rounded-full blur-[120px] opacity-60 mix-blend-screen animate-pulse duration-[8000ms]"></div>
+        <div className="absolute bottom-[-20%] right-[-10%] w-[60vw] h-[60vw] bg-custom-orange/10 rounded-full blur-[120px] opacity-60 mix-blend-screen animate-pulse duration-[10000ms] delay-1000"></div>
+      </div>
+
+      <CustomCursor />
+
+      <main className="relative w-full z-10">
+        <FeaturedProjects />
+      </main>
+    </div>
+  )
+}
