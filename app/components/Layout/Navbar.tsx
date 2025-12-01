3345de0a29c69addec53e8ef3218c465554b1fe5
Poyuan <poyuan510214@gmail.com>
2025-12-01 16:44:33 +0800

    style: disable mix-blend-difference on mobile

--- app/components/Layout/Navbar.tsx
+++ app/components/Layout/Navbar.tsx
@@ -81,7 +81,7 @@ export const Navbar: React.FC = () => {
     <>
       <nav
         ref={navRef}
-        className="fixed top-0 left-0 w-full flex justify-between items-center px-6 md:px-12 py-6 z-[60] text-white mix-blend-difference backdrop-blur-lg border-b border-white/5 bg-custom-black/5"
+        className="fixed top-0 left-0 w-full flex justify-between items-center px-6 md:px-12 py-6 z-[60] text-custom-white md:text-white bg-black/60 md:bg-transparent md:mix-blend-difference backdrop-blur-lg border-b border-black/10"
       >
         <div
           ref={logoRef}
